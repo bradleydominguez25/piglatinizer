cc47755bb5c5c19596da02c8ccadb38019846d6d
Bradley Dominguez <bradleydominguez25@gmail.com>
2018-01-18 23:14:54 +0000

    Yerr

--- js/script.js
+++ js/script.js
@@ -5,22 +5,23 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
-	var type = $("#input").val();
-	$("button").click(function(){
-  	$("#result").html(type + "ayy")
-  });
+$(document).ready(function() {
+		var type = $("#input").val();
+			$("button").click(function() {
+				var type = $("#input").val();
+				$("#result").append(type + "ayy")
+			});
+		var each = type.split(" ");
+		
+
+});
+			// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
 
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
 
 
 
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+			// Create the sentenceToPigLatin function that takes a sentence as a parameter
+			//Loops through all the words in the sentence and transforms each word
+			//It should return a transfromed sentance
